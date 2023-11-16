0a2e452106a5747eb633c3a727b4d9367c3aec48
Prakash S <79446554+prakashs-dev@users.noreply.github.com>
2023-11-16 09:34:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 const makeChange = (c) => {
   // your name here
+	let coin{
+		q: 25,
+		d: 10,
+		n: 5,
+		p: 1
+	};
+	let obj = {};
+	for(let i in coin){
+		let currentItem = coin[i];
+		obj[i] = parseInt(c / currentItem);
+		c = c % currentItem;
+	}
+	return obj;
 };
 
 // Do not the change the code below
